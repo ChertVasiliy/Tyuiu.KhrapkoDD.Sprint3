469d4e8ce3f90d1db98a47f381e2c892fcbee9e0
ChertVasiliy <kolyaminigan21@gmail.com>
2024-10-28 11:15:14 +0500

    ,

--- Tyuiu.KhrapkoDD.Sprint3.Task1.V8.Lib/DataService.cs
+++ Tyuiu.KhrapkoDD.Sprint3.Task1.V8.Lib/DataService.cs
@@ -6,13 +6,13 @@ namespace Tyuiu.KhrapkoDD.Sprint3.Task1.V8.Lib
     {
         public double GetSumSeries(double value, int startValue, int stopValue)
         {
-            double P = 1;
+            double SumSeries = 1;
             while (startValue <= stopValue)
             {
-                P = P * Math.Pow(1 / (Math.Cos(value) + Math.Pow(value, startValue)), startValue);
+                SumSeries *= Math.Pow((1 / (Math.Cos(startValue) + Math.Pow(value, startValue))), startValue);
                 startValue++;
             }
-            return Math.Round(P, 3);
+            return Math.Round(SumSeries, 3);
         }
         //////
     }
