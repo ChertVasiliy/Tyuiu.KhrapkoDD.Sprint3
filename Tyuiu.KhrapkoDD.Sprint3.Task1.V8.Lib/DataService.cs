af905613c2743dad66f07c21cdbbb2a86f989e74
ChertVasiliy <kolyaminigan21@gmail.com>
2024-10-28 11:12:06 +0500

    ш

--- Tyuiu.KhrapkoDD.Sprint3.Task1.V8.Lib/DataService.cs
+++ Tyuiu.KhrapkoDD.Sprint3.Task1.V8.Lib/DataService.cs
@@ -9,11 +9,11 @@ namespace Tyuiu.KhrapkoDD.Sprint3.Task1.V8.Lib
             double P = 1;
             while (startValue <= stopValue)
             {
-                P = P * Math.Pow(1 / Math.Cos(value + Math.Pow(value, startValue)), startValue);
+                P = P * Math.Pow(1 / Math.Cos(value) + Math.Pow(value, startValue), startValue);
                 startValue++;
             }
             return Math.Round(P, 3);
         }
-        ////
+        //////
     }
 }
